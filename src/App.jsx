24a6fbd03907839be6170dedcddc91dd1e0a125f
Santiago Guzman <sanguzsua20@gmail.com>
2025-08-31 21:29:28 +0000

    cambio el estilo y se agregan iconos de los sponsors

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import React from "react";
 
-function Aplicaciones() {
-  return (
-    <div id="a">
-      <a
-        href="https://play.google.com/store/apps/details?id=com.mcentric.mcclient.MyMadrid&hl=es"
-        target="_blank"
-      >
-        <img src="src/assets/google-play-2.svg" alt="Google Play" />
-      </a>
 
-      <a
-        href="https://apps.apple.com/"
-        target="_blank"
-      >
-        <img src="src/assets/app-store-2.svg" alt="App Store" />
-      </a>
+function App() {
+const appSection = {
+   titulo: "Descarga ahora",
+   subtitulo: "Real Madrid App",
+  
+ 
+ nombre: "Google Play",
+ link: "https://play.google.com/store",
+ img1: "src/assets/google-play-2.svg",
+ 
+ nombre: "App Store",
+ link: "https://apps.apple.com/",
+ img2: "src/assets/app-store-2.svg",
 
-      <a
-        href="https://appgallery.huawei.com/"
-        target="_blank"
-      >
-        <img src="src/assets/huawei-store-2.svg" alt="Huawei AppGallery" />
-      </a>
-    </div>
-  );
+
+ nombre: "Huawei AppGallery",
+ link: "https://appgallery.huawei.com/",
+ img3: "src/assets/huawei-store-2.svg",
+
+
+ nombre: "Emirates",
+ link: "https://www.emirates.com/es/spanish/",
+ img4: "src/assets/emirates_gray.svg",
+
+ nombre: "Adidas",
+ link: "https://www.adidas.es/futbol",
+ img5: "src/assets/adidas_gray.svg",
+
+ nombre: "HP",
+ link: "https://www.hp.com/es-es/home.html",
+ img6: "src/assets/hp_gray.svg"
+ 
+ };
+ 
+ return (
+ <>
+ <div className="divider"></div>
+
+ <p>{appSection.titulo}</p>
+ <h2>{appSection.subtitulo}</h2>
+
+ <div className="divider"></div>
+
+ <img src={appSection.img1}/>
+ <img src={appSection.img2}/>
+ <img src={appSection.img3}/>
+
+<div className="divider"></div>
+
+ <div className="sponsors-logos">
+ <img src={appSection.img4}/>
+ <img src={appSection.img5}/>
+ </div>
+
+ <div className="hp">
+ <img src={appSection.img6}/>
+ </div>
+ </> 
+ );
 }
-export default function Gallery() {
-  return (
-    <section>
-      <p>Descarga ahora</p>
-      <h2>Real Madrid App</h2>
-      <Aplicaciones />
-      </section>
-  );
-}
+
+export default App;
